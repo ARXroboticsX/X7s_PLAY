b05008431141a610ed5fc7432b6504060035fc01
du-xinyi <386476500@qq.com>
2025-07-11 14:09:16 +0800

    更新脚本

--- collect.sh
+++ collect.sh
@@ -21,15 +21,11 @@ check_executable() {
     fi
 }
 
-check_path "${workspace}/X7s/00-sh/ROS"
 check_path "${workspace}/X7s_PLAY/realsense_camera"
 check_path "${workspace}/X7s_PLAY/mobile_aloha"
 
-check_executable "${workspace}/X7s/00-sh/ROS/remote_X7s.sh"
 check_executable "${workspace}/X7s_PLAY/realsense_camera/realsense.sh"
 
-gnome-terminal --title="x7" -- bash -c "cd ${workspace}/X7s/00-sh/ROS; bash remote_X7s.sh; exec bash"
-sleep 1
 gnome-terminal --title="realsense" -- bash -c "cd ${workspace}/X7s_PLAY/realsense_camera; bash realsense.sh; exec bash"
 sleep 1
 gnome-terminal --title="collect" -- bash -c "cd ${workspace}/X7s_PLAY/mobile_aloha/; source ./venv/bin/activate; \
--- inference.sh
+++ inference.sh
@@ -21,29 +21,11 @@ check_executable() {
     fi
 }
 
-check_path "${workspace}/X7s/ARX_CAN/arx_can"
-check_path "${workspace}/X7s/body/ROS"
-check_path "${workspace}/X7s/x7s/ROS/x7s_ws"
 check_path "${workspace}/X7s_PLAY/realsense_camera"
 check_path "${workspace}/X7s_PLAY/mobile_aloha"
 
-check_executable "${workspace}/X7s/ARX_CAN/arx_can/arx_can1.sh"
-check_executable "${workspace}/X7s/ARX_CAN/arx_can/arx_can3.sh"
-check_executable "${workspace}/X7s/ARX_CAN/arx_can/arx_can5.sh"
 check_executable "${workspace}/X7s_PLAY/realsense_camera/realsense.sh"
 
-gnome-terminal --title="can1" -x bash -c "cd ${workspace}/X7s/ARX_CAN/arx_can; bash arx_can1.sh; exec bash"
-sleep 1
-gnome-terminal --title="can3" -x bash -c "cd ${workspace}/X7s/ARX_CAN/arx_can; bash arx_can3.sh; exec bash"
-sleep 1
-gnome-terminal --title="can5" -x bash -c "cd ${workspace}/X7s/ARX_CAN/arx_can; bash arx_can5.sh; exec bash"
-sleep 1
-gnome-terminal --title="body" -- bash -c "cd ${workspace}/X7s/body/ROS; source ./devel/setup.bash && roslaunch arx_lift_controller x7s.launch; exec bash"
-sleep 1
-gnome-terminal --title="left_arm" -- bash -c "cd ${workspace}/X7s/x7s/ROS/x7s_ws; source ./devel/setup.bash && roslaunch arx_x7_controller left_arm_inference.launch; exec bash"
-sleep 1
-gnome-terminal --title="right_arm" -- bash -c "cd ${workspace}/X7s/x7s/ROS/x7s_ws; source ./devel/setup.bash && roslaunch arx_x7_controller right_arm_inference.launch; exec bash"
-sleep 1
 gnome-terminal --title="realsense" -- bash -c "cd ${workspace}/X7s_PLAY/realsense_camera; bash realsense.sh; exec bash"
 sleep 1
 gnome-terminal --title="inference" -- bash -c "cd ${workspace}/X7s_PLAY/mobile_aloha; source ./venv/bin/activate; \
